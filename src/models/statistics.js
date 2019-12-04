fa5df43f1aec9324890a8ef08c69a6d309b3df96
guangtuan <zhuangguangtuan@gmail.com>
2019-12-04 09:11:26 +0800

    feat: add summary amount at stat page. add date range query at stat page

--- src/models/statistics.js
+++ src/models/statistics.js
@@ -1,13 +1,22 @@
 import { post, get } from '../core/request';
 import * as R from 'ramda';
+import dayjs from 'dayjs';
 
 export const statistics = {
     state: {
-        content: []
+        content: [],
+        dateRange: [
+            dayjs().subtract(1, 'month').set("date", 1).toDate(),
+            dayjs().set("date", 1).set("date", 0).toDate()
+        ]
     },
     reducers: {
         setContent: (state, payload) => {
             return R.assoc('content', payload)(state);
+        },
+        setDateRange: (state, payload) => {
+            state.dateRange = payload
+            return state
         }
     },
     effects: dispatch => ({
