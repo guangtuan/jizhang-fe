aa5f4de5194715a73c72df5c0031d1f478c02810
guangtuan <zhuangguangtuan@gmail.com>
2020-01-02 21:42:52 +0800

    feat: set default date range to start of month and end of today in stat page

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import { BrowserRouter, Route, Redirect } from "react-router-dom";
-import {Provider} from 'react-redux';
+import { Provider } from 'react-redux';
 import selectPlugin from '@rematch/select';
-import {init} from '@rematch/core';
+import { init } from '@rematch/core';
 import App from './pages/app';
 import * as models from './models';
 import './index.css';
@@ -14,8 +13,8 @@ const store = init({
 });
 
 ReactDOM.render(
-    <Provider store={store}>
-      <App />
-    </Provider>,
-    document.getElementById('root'),
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
 );
