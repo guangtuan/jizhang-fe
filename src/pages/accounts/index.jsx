2f14f475993795b2bdeb696e71bc570a50dc680c
guangtuan <zhuangguangtuan@gmail.com>
2020-09-21 11:21:13 +0800

    refactor: account manage page use material-ui, todo: add account dialog

--- src/pages/accounts/index.jsx
+++ src/pages/accounts/index.jsx
@@ -1,8 +1,54 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
-import { Table, Button, Dialog, Form, Input, Select } from 'element-react';
 import * as R from 'ramda';
 import styles from './accounts.module.css';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import AddIcon from '@material-ui/icons/Add';
+import Fab from '@material-ui/core/Fab';
+import { makeStyles } from '@material-ui/core/styles';
+
+
+const useStyles = makeStyles((theme) => ({
+    table: {
+        minWidth: 650,
+    },
+    container: {
+        maxHeight: 700,
+    },
+    opt: {
+        margin: theme.spacing(1),
+    },
+    fab: {
+        position: 'absolute',
+        bottom: theme.spacing(4),
+        right: theme.spacing(2),
+    },
+    formControl: {
+        margin: theme.spacing(1),
+        minWidth: 120,
+        maxWidth: 300,
+    },
+    chips: {
+        display: 'flex',
+        flexWrap: 'wrap',
+    },
+    chip: {
+        margin: 2,
+    },
+    noLabel: {
+        marginTop: theme.spacing(3),
+    },
+    backdrop: {
+        zIndex: theme.zIndex.drawer + 1,
+        color: '#fff',
+    },
+}));
 
 function Accounts({
     users,
@@ -12,14 +58,16 @@ function Accounts({
     showDialog, hideDialog, changeProperty, clearForm
 }) {
 
+    const classes = useStyles();
+
     const columns = [
         {
             label: '账户类型',
             render: function ({ type }) {
                 return (
-                    <span>
-                        <span>{R.prop("name")(R.find(R.propEq('value', type))(accountTypeDefine))}</span>
-                    </span>
+                    <TableCell>
+                        {R.prop("name")(R.find(R.propEq('value', type))(accountTypeDefine))}
+                    </TableCell>
                 )
             }
         },
@@ -43,20 +91,40 @@ function Accounts({
 
     return (
         <div>
-            <Table
-                defaultExpandAll={true}
-                columns={columns}
-                data={accounts}
-                border={true}
-                rowKey={R.prop('id')}
-            />
-            <Button
-                className={styles.add}
-                type="primary"
-                onClick={showDialog}>
-                添加账户
-            </Button>
-            <Dialog
+            <Paper>
+                <TableContainer className={classes.container} component={Paper}>
+                    <Table stickyHeader className={classes.table} aria-label="simple table" >
+                        <TableHead>
+                            <TableRow>
+                                {columns.map(R.prop('label')).map((item) => {
+                                    return (<TableCell key={item}>{item}</TableCell>)
+                                })}
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {
+                                accounts.map(account => {
+                                    return <TableRow>{
+                                        columns.map(col => {
+                                            if (col.render) {
+                                                return col.render(account);
+                                            } else {
+                                                return <TableCell>{R.prop(col.prop)(account)}</TableCell>
+                                            }
+                                        })
+                                    }</TableRow>
+                                }
+                                )
+                            }
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </Paper>
+            
+            <Fab aria-label="Add" className={classes.fab} color={"primary"} onClick={showDialog}>
+                <AddIcon />
+            </Fab>
+            {/* <Dialog
                 title="添加账户"
                 size="tiny"
                 visible={accountCreation.dialogVisibility}
@@ -128,7 +196,7 @@ function Accounts({
                         </Form.Item>
                     </Form>
                 </Dialog.Body>
-            </Dialog>
+            </Dialog> */}
         </div>
     )
 
