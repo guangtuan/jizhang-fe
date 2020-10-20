9ad062ead01f1fd3b644be720631c399b0f29c7a
guangtuan <zhuangguangtuan@gmail.com>
2020-10-20 22:29:53 +0800

    style: format code in subjectSelector and userSelector

--- src/comp/userSelector.jsx
+++ src/comp/userSelector.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import {connect} from 'react-redux';
 import * as R from 'ramda';
 
-import { makeStyles } from '@material-ui/core/styles';
+import {makeStyles} from '@material-ui/core/styles';
 
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
@@ -10,62 +10,62 @@ import FormControl from '@material-ui/core/FormControl';
 import InputLabel from '@material-ui/core/InputLabel';
 
 const emptyItem = () => ({
-    id: undefined,
-    name: "清空"
+  id: undefined,
+  name: '清空',
 });
 
 const ITEM_HEIGHT = 48;
 const ITEM_PADDING_TOP = 8;
 const MenuProps = {
-    PaperProps: {
-        style: {
-            maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-            width: 250,
-        },
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
     },
+  },
 };
 
 const useStyles = makeStyles((theme) => ({
-    formControl: {
-        margin: theme.spacing(1),
-        minWidth: 120,
-        maxWidth: 300,
-    },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+    maxWidth: 300,
+  },
 }));
 
 function UserSelector({
-    users,
-    title,
-    onChange,
-    value,
-    multiple
+  users,
+  title,
+  onChange,
+  value,
+  multiple,
 }) {
-    const classes = useStyles();
-    const toSelect = R.concat([emptyItem()], users);
-    const toSelectItem = user => <MenuItem key={user.id} value={user.id}>{user.nickname}</MenuItem>;
-    return (
-        <FormControl className={classes.formControl}>
-            <InputLabel id="label_desc_account">{title}</InputLabel>
-            <Select
-                multiple={!!multiple}
-                MenuProps={MenuProps}
-                value={value}
-                onChange={event => {
-                    const val = event.target.value
-                    if (!multiple) {
-                        onChange(val)
-                        return
-                    }
-                    if (val.some(id => id === undefined)) {
-                        onChange([])
-                        return
-                    }
-                    onChange(val)
-                }}>
-                {toSelect.map(toSelectItem)}
-            </Select>
-        </FormControl>
-    )
+  const classes = useStyles();
+  const toSelect = R.concat([emptyItem()], users.content);
+  const toSelectItem = (user) => <MenuItem key={user.id} value={user.id}>{user.nickname}</MenuItem>;
+  return (
+    <FormControl className={classes.formControl}>
+      <InputLabel id="label_desc_account">{title}</InputLabel>
+      <Select
+        multiple={!!multiple}
+        MenuProps={MenuProps}
+        value={value}
+        onChange={(event) => {
+          const val = event.target.value;
+          if (!multiple) {
+            onChange(val);
+            return;
+          }
+          if (val.some((id) => id === undefined)) {
+            onChange([]);
+            return;
+          }
+          onChange(val);
+        }}>
+        {toSelect.map(toSelectItem)}
+      </Select>
+    </FormControl>
+  );
 }
 
 const mapState = R.pick(['users']);
