f0d0fd6739db76ae43e38ddb2e637b928aae9bee
guangtuan <zhuangguangtuan@gmail.com>
2020-12-10 21:42:08 +0800

    refactor: use AutoComplete on UserSelector

--- src/comp/userSelector.jsx
+++ src/comp/userSelector.jsx
@@ -1,25 +1,18 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import * as R from 'ramda';
-import Select from '@material-ui/core/Select';
-import MenuItem from '@material-ui/core/MenuItem';
-import InputLabel from '@material-ui/core/InputLabel';
+import {
+  prop,
+  find,
+  propEq,
+  map,
+  defaultTo,
+  pick,
+  compose,
+  nthArg
+} from 'ramda';
 
-const emptyItem = () => ({
-  id: undefined,
-  name: '清空',
-});
-
-const ITEM_HEIGHT = 48;
-const ITEM_PADDING_TOP = 8;
-const MenuProps = {
-  PaperProps: {
-    style: {
-      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-      width: 250,
-    },
-  },
-};
+import { TextField } from '@material-ui/core';
+import { Autocomplete } from '@material-ui/lab';
 
 function UserSelector({
   users,
@@ -28,33 +21,29 @@ function UserSelector({
   value,
   multiple
 }) {
-  const toSelect = R.concat([emptyItem()], users.content);
-  const toSelectItem = (user) => <MenuItem key={title + user.id} value={user.id}>{user.nickname}</MenuItem>;
-  return (
-    <React.Fragment>
-      <InputLabel>{title}</InputLabel>
-      <Select
-        multiple={!!multiple}
-        MenuProps={MenuProps}
-        value={value}
-        onChange={(event) => {
-          const val = event.target.value;
-          if (!multiple) {
-            onChange(val);
-            return;
-          }
-          if (val.some((id) => id === undefined)) {
-            onChange([]);
-            return;
-          }
-          onChange(val);
-        }}>
-        {toSelect.map(toSelectItem)}
-      </Select>
-    </React.Fragment>
-  );
+
+  return <Autocomplete
+    multiple={multiple}
+    disableCloseOnSelect={multiple}
+    defaultValue={find(propEq('id', value))(users.content)}
+    options={users.content}
+    getOptionLabel={prop('nickname')}
+    onChange={
+      multiple ?
+        compose(onChange, map(prop('id')), defaultTo([]), nthArg(1)) :
+        compose(onChange, prop('id'), nthArg(1))
+    }
+    renderInput={(params) => (
+      <TextField
+        {...params}
+        variant="standard"
+        label={title}
+        placeholder={title}
+      />
+    )}
+  />
 }
 
-const mapState = R.pick(['users']);
+const mapState = pick(['users']);
 
 export default connect(mapState)(UserSelector);
