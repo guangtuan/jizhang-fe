456210314bcd2f5463304e4e85d34a839d8f3d97
guangtuan <zhuangguangtuan@gmail.com>
2020-12-12 16:22:18 +0800

    refactor: AccountSelector use AutoComplete

--- src/comp/accountSelector.jsx
+++ src/comp/accountSelector.jsx
@@ -1,9 +1,49 @@
-import createSelector from './SelectorCreator';
-import * as R from 'ramda';
+import React from 'react';
+import { connect } from 'react-redux';
+import {
+  prop,
+  find,
+  propEq,
+  map,
+  defaultTo,
+  pick,
+  compose,
+  nthArg
+} from 'ramda';
 
-export default createSelector({
-    display: R.prop('name'),
-    id: R.prop('id'),
-    clearable: true,
-    stateKey: "accounts",
-});
+import { TextField } from '@material-ui/core';
+import { Autocomplete } from '@material-ui/lab';
+
+function AccountSelector({
+  accounts,
+  title,
+  onChange,
+  value,
+  multiple
+}) {
+
+  return <Autocomplete
+    multiple={multiple}
+    disableCloseOnSelect={multiple}
+    defaultValue={find(propEq('id', value))(accounts)}
+    options={accounts}
+    getOptionLabel={prop('name')}
+    onChange={
+      multiple ?
+        compose(onChange, map(prop('id')), defaultTo([]), nthArg(1)) :
+        compose(onChange, prop('id'), nthArg(1))
+    }
+    renderInput={(params) => (
+      <TextField
+        {...params}
+        variant="standard"
+        label={title}
+        placeholder={title}
+      />
+    )}
+  />
+}
+
+const mapState = pick(['accounts']);
+
+export default connect(mapState)(AccountSelector);
