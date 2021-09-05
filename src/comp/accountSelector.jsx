36f80d86b8dcb7065e8915013b0a4287a14d57ad
zhuangguangtuan <zhuangguangtuan@gmail.com>
2021-09-05 20:57:09 +0800

    style: format code

--- src/comp/accountSelector.jsx
+++ src/comp/accountSelector.jsx
@@ -1,54 +1,54 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import {
-  prop,
-  find,
-  propEq,
-  map,
-  defaultTo,
-  pick,
-  compose,
-  nthArg
+    prop,
+    find,
+    propEq,
+    map,
+    defaultTo,
+    pick,
+    compose,
+    nthArg
 } from 'ramda';
 
 import { TextField } from '@material-ui/core';
 import { Autocomplete } from '@material-ui/lab';
 
-function AccountSelector({
-  accounts,
-  title,
-  onChange,
-  value,
-  multiple
+function AccountSelector ({
+    accounts,
+    title,
+    onChange,
+    value,
+    multiple
 }) {
 
-  const optionLabel = ({name, tail}) => {
-    if (tail) {
-      return `${name}(${tail})`;
+    const optionLabel = ({ name, tail }) => {
+        if (tail) {
+            return `${name}(${tail})`;
+        }
+        return name;
     }
-    return name;
-  }
 
-  return <Autocomplete
-    multiple={multiple}
-    disableCloseOnSelect={multiple}
-    defaultValue={find(propEq('id', value))(accounts)}
-    options={accounts}
-    getOptionLabel={optionLabel}
-    onChange={
-      multiple ?
-        compose(onChange, map(prop('id')), defaultTo([]), nthArg(1)) :
-        compose(onChange, prop('id'), nthArg(1))
-    }
-    renderInput={(params) => (
-      <TextField
-        {...params}
-        variant="standard"
-        label={title}
-        placeholder={title}
-      />
-    )}
-  />
+    return <Autocomplete
+        multiple={multiple}
+        disableCloseOnSelect={multiple}
+        defaultValue={find(propEq('id', value))(accounts)}
+        options={accounts}
+        getOptionLabel={optionLabel}
+        onChange={
+            multiple ?
+                compose(onChange, map(prop('id')), defaultTo([]), nthArg(1)) :
+                compose(onChange, prop('id'), nthArg(1))
+        }
+        renderInput={(params) => (
+            <TextField
+                {...params}
+                variant="standard"
+                label={title}
+                placeholder={title}
+            />
+        )}
+    />
 }
 
 const mapState = pick(['accounts']);
